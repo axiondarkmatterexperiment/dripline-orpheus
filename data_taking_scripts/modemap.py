659f9ff337b08db4fd6010432747ae99a3296774
Raphael Cervantes <raphaelc@uw.edu>
2020-06-16 08:12:48 -0700

    functioning modemap script

--- data_taking_scripts/modemap.py
+++ data_taking_scripts/modemap.py
@@ -1,8 +1,8 @@
 from dripline.core import Interface
+import time
 
 auths_file = '/etc/rabbitmq-secret/authentications.json'
 the_interface = Interface(dripline_config={'auth-file': auths_file})
-print(test_interface.get('peaches'))
 
 #move curved mirror to 0 position.
 the_interface.set('curved_mirror_set_position', 0)
@@ -12,9 +12,19 @@ the_interface.set('modemap_measurement_status', 'start_measurement')
 
 
 for i in range(10):
+    print(i)
     the_interface.set('na_measurement_status', 'start_measurement')
-    the_interface.cmd('na_snapshot', '-s log_entities')
-    the_interface.set('curved_mirror_move_steps', '10')
+    time.sleep(2)
+    print('help1')
+    the_interface.cmd('na_snapshot', ' log_entities')
+    print('help2')
+    time.sleep(2)
+    the_interface.set('curved_mirror_move_steps', 10)
+    print('help3')
+    time.sleep(2)
     the_interface.set('na_measurement_status', 'stop_measurement')
+    print('help4')
+    time.sleep(2)
 
 
+the_interface.set('modemap_measurement_status', 'stop_measurement')
