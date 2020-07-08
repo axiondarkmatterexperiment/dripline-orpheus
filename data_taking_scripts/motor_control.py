f5fcde24e18bbde243f55372542f1586275966ab
Parashar Mohapatra <parashar2399@gmail.com>
2020-07-07 18:25:36 -0700

    added backpedalling

--- data_taking_scripts/motor_control.py
+++ data_taking_scripts/motor_control.py
@@ -12,10 +12,12 @@ def get_status():
     return [curved_mirror_status,top_plate_status,bottom_plate_status]
 
 #distance to move
+print("PLEASE ENTER DISTANCE IN MILLIMETERS (mm)")
 distance_to_move = float(input('Enter the distance to move in inches (Empty cavity modemap is usually 1.18): '))
 resolution = float(input('Enter the number of measurements needed: '))
 increment_distance = distance_to_move/resolution
-
+forward_increment = increment_distance+increment_distance/2
+back_increment = (-1)*increment_distance/2
 #time to wait for motor to move
 sleep2 = 2
 sleep5 = 5
@@ -51,14 +53,23 @@ gap = holder_center - plate_center
 
 #functions to convert distances into steps
 def plates_distance_to_steps(distance):
-    actual_distance = distance + gap
-    num_pitch_lengths = actual_distance/pitch #these many complete rotations
+    distance_in_inch = distance/25.4
+    num_pitch_lengths = distance_in_inch/pitch #these many complete rotations
     steps = steps_per_rotation * num_pitch_lengths
     return int(round(steps))
 def curved_mirror_distance_to_steps(distance):
-    num_pitch_lengths = distance/pitch #these many complete rotations
+    distance_in_inch = distance/25.4
+    num_pitch_lengths = distance_in_inch/pitch #these many complete rotations
     steps = steps_per_rotation * num_pitch_lengths
     return int(round(steps))
+def bottom_dielectric_dist(cavity_len, incr, initial_separation):
+    cavity_len = cavity_len+incr
+    new_separation = cavity_len/(num_plates+1)
+    diff = initial_separation + incr
+    move_bottom_plate = diff - new_separation
+    return move_bottom_plate, new_separation, cavity_len
+
+
 #Setting cavity length to 6.3 inches for now.
 #In practice this wil be something like the_interface.get(steps)
 cavity_length_tracker = 6.3
@@ -76,13 +87,10 @@ while i <= distance_to_move:
     #Logging list of endpoints
     the_interface.cmd('na_snapshot', 'log_entities')
     #moving curved mirror
-    print('Moving curved mirror motor by {} steps'.format(curved_mirror_distance_to_steps(increment_distance)))
-    the_interface.set('curved_mirror_move_steps', curved_mirror_distance_to_steps(increment_distance))
+    print('Moving curved mirror motor by {} steps'.format(curved_mirror_distance_to_steps(forward_increment)))
+    the_interface.set('curved_mirror_move_steps', curved_mirror_distance_to_steps(forward_increment))
     #adjusting bottom dielectric plate
-    cavity_length_tracker = cavity_length_tracker+increment_distance
-    new_plate_separation = (cavity_length_tracker/(num_plates+1))
-    diff = initial_plate_separation + increment_distance
-    move_bottom_plate = diff - new_plate_separation #distance to move bottom dielectric plate
+    move_bottom_plate, new_plate_separation, cavity_length_tracker = bottom_dielectric_dist(cavity_length_tracker, forward_increment, initial_plate_separation)
     print('Moving bottom plate motor by {} steps'.format(plates_distance_to_steps(move_bottom_plate)))
     the_interface.set('bottom_dielectric_plate_move_steps', plates_distance_to_steps(move_bottom_plate))
     #adjusting top dielectric plate
@@ -93,11 +101,27 @@ while i <= distance_to_move:
     while(get_status() != ['R', 'R', 'R']):
         print(get_status())
     print('done with checking request. exited while loop')
+    initial_plate_separation = new_plate_separation
     time.sleep(sleep5)
+    #backpedalling
+    print('Moving curved mirror motor by {} steps'.format(curved_mirror_distance_to_steps(back_increment)))
+    the_interface.set('curved_mirror_move_steps', curved_mirror_distance_to_steps(back_increment))
+    move_bottom_plate, new_plate_separation, cavity_length_tracker = bottom_dielectric_dist(cavity_length_tracker, back_increment, initial_plate_separation)
+    the_interface.set('curved_mirror_move_steps', curved_mirror_distance_to_steps(move_bottom_plate))
+    #adjusting top dielectric plate
+    move_top_plate = new_plate_separation - initial_plate_separation
+    print('Moving top plate motor by {} steps'.format(plates_distance_to_steps(move_top_plate)))
+    the_interface.set('top_dielectric_plate_move_steps', plates_distance_to_steps(move_top_plate))
+
+    #wait for motor. If program gets stuck check here for infinite loop
+    while(get_status() != ['R', 'R', 'R']):
+        print(get_status())
+    print('done with checking request. exited while loop')
+
     print('Setting na_measurement_status to stop_measurement')
     the_interface.set('na_measurement_status', 'stop_measurement')
     time.sleep(sleep2)
-    i = round((i+increment_distance),4)
+    i = round((i+forward_increment+back_increment),4)
     initial_plate_separation = new_plate_separation
     print("now scanning distance = " +str(i))
 
