cd1a46d14f1dc2e89e00bdb6ff8c1f9c1082fbbc
Parashar Mohapatra <parashar2399@gmail.com>
2020-07-11 13:56:35 -0700

    bug fixes and additional functionality

--- data_taking_scripts/testing_class.py
+++ data_taking_scripts/testing_class.py
@@ -1,10 +1,8 @@
-from dripline.core import Interface
 import time
 import common_functions
 from motor import OrpheusMotors
 #setting up connection to dripline
 auths_file = '/etc/rabbitmq-secret/authentications.json'
-the_interface = Interface(dripline_config={'auth-file': auths_file})
 
 #set up motors
 orpheus_motors = OrpheusMotors(auths_file)
