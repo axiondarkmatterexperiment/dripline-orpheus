1f7db1611dec25befb57ac4ff8f78966542f06ce
Parashar Mohapatra <parashar2399@gmail.com>
2020-08-26 11:28:29 -0700

    fixed increment error. changed variable names

--- data_taking_scripts/motor_control.py
+++ data_taking_scripts/motor_control.py
@@ -18,7 +18,7 @@ wide_scan_stop_freq = 18e9
 narrow_scan_span = 200e6
 sec_wait_for_na_averaging = 2
 
-initial_mirror_holder_spacing = cm_to_inch(float(input('Enter initial mirror holder spacing (in cm): ')))
+initial_mirror_holder_spacing = float(input('Enter initial mirror holder spacing (in cm): '))
 distance_to_move = float(input('Enter the distance to move in cm (Empty resonator modemap is usually 3): '))
 resolution = int(input('Enter the number of measurements needed: '))
 increment_distance = cm_to_inch(distance_to_move/resolution)
@@ -35,14 +35,14 @@ measurement_description = input('Describe the current measurement setup: ')
 logger.initialize_na_settings_for_modemap(averages = 16)
 orpheus_motors.move_to_zero()
 orpheus_motors.wait_for_motors()
-mirror_spacing_tracker = initial_mirror_holder_spacing ### CHECK
-current_plate_separation = orpheus_motors.plate_separation(mirror_spacing_tracker,num_plates)
 
 #Send alert saying you are starting the modemap measurement
 print('Starting modemap measurement')
 logger.start_modemap(measurement_description)
 
-current_resonator_length = inch_to_cm(initial_mirror_holder_spacing)+1.0497
+current_resonator_length_cm = initial_mirror_holder_spacing+1.0497
+current_resonator_length_in = cm_to_inch(current_resonator_length_cm)
+current_plate_separation = orpheus_motors.plate_separation(current_resonator_length_in,num_plates)
 try:
     delta_length = 0
     override = 0 # 0 is false, 1 is true
@@ -54,8 +54,8 @@ try:
             if prompt == 'o':
                 override = 1
 
-        print('Resonator length: {}'.format(current_resonator_length))
-        resonant_freq = logger.flmn(0,0,18,current_resonator_length)
+        print('Resonator length: {}'.format(current_resonator_length_cm))
+        resonant_freq = logger.flmn(0,0,18,current_resonator_length_cm)
         if narrow_scan:
             narrow_scan_start_freq = resonant_freq - narrow_scan_span/2
             narrow_scan_stop_freq = resonant_freq + narrow_scan_span/2
@@ -67,12 +67,12 @@ try:
             logger.log_modemap(narrow_scan_start_freq, narrow_scan_stop_freq, sec_wait_for_na_averaging, 'narrowscan')
 
         print("now scanning distance = " +str(delta_length))
-        mirror_spacing_tracker, new_plate_separation = orpheus_motors.move_by_increment(increment_distance,
-                                                                                        mirror_spacing_tracker,
-                                                                                        num_plates,
-                                                                                        current_plate_separation)
+        current_resonator_length_in, new_plate_separation = orpheus_motors.move_by_increment(increment_distance,
+                                                                                             current_resonator_length_in,
+                                                                                             num_plates,
+                                                                                             current_plate_separation)
         orpheus_motors.wait_for_motors()
-        current_resonator_length = current_resonator_length+inch_to_cm(increment_distance)
+        current_resonator_length_cm = current_resonator_length_cm+inch_to_cm(increment_distance)
         current_plate_separation = new_plate_separation
         print("plate separation: {}".format(current_plate_separation))
 
