0c5dfd8c2a2e5216de1f4265c38df39a32c6b486
Raphael Cervantes <raphaelc@uw.edu>
2021-03-02 10:10:09 -0800

    fixed endpoint names

--- data_taking_scripts/kill_motors.py
+++ data_taking_scripts/kill_motors.py
@@ -15,5 +15,5 @@ auths_file = '/etc/rabbitmq-secret/authentications.json'
 the_interface = Interface(dripline_config={'auth-file': auths_file})
 
 the_interface.set('curved_mirror_status_command', 'stop_and_kill')
-the_interface.set('top_dielectric_plate_command', 'stop_and_kill')
-the_interface.set('bottom_dielectric_plate_command', 'stop_and_kill')
+the_interface.set('top_dielectric_plate_status_command', 'stop_and_kill')
+the_interface.set('bottom_dielectric_plate_status_command', 'stop_and_kill')
