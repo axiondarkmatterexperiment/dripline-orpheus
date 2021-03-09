9d8a30246031a764b8becb96512e5afc868fae08
Raphael Cervantes <raphaelc@uw.edu>
2021-03-09 10:23:02 -0800

    option for realtime fitting

--- data_taking_scripts/data_logging.py
+++ data_taking_scripts/data_logging.py
@@ -1,6 +1,8 @@
 from dripline.core import Interface
 import time
 import math
+
+
 class DataLogger:
 
     def __init__(self, auths_file):
@@ -61,7 +63,7 @@ class DataLogger:
         print('Setting na_measurement_status to stop_measurement')
         self.cmd_interface.set('na_measurement_status', 'stop_measurement')
 
-    def log_transmission_reflection_switches(self,start_freq, stop_freq, sec_wait_for_na_averaging, na_iq_data_notes= '', autoscale = False):
+    def log_transmission_reflection_switches(self,start_freq, stop_freq, sec_wait_for_na_averaging, na_iq_data_notes= '', autoscale = False, fitting = False):
         self.set_start_freq(start_freq)
         self.set_stop_freq(stop_freq)
         print('Setting na_measurement_status to start_measurement')
@@ -77,6 +79,16 @@ class DataLogger:
         if autoscale:
             self.cmd_interface.set('na_commands', 'autoscale')
         self.cmd_interface.cmd('s21_iq_transmission_data', 'scheduled_log')
+        if fitting:
+            s21_iq = self.cmd_interface.get('s21_iq_transmission_data').payload.to_python()('value_cal')
+            s21_re, s21_im = array(s21_iq[::2]), array(s21_iq[1::2])
+            s21_pow = s21_re**2 + s21_im**2
+            freq = np.linspace(start_freq, stop_freq, num = len(s21_pow))
+            popt_transmission, pcov_transmission = data_lorentzian_fit(pow_s21, freq_mag_s21, 'transmission')
+            perr_transmission = np.sqrt(np.diag(pcov_transmission))
+            print('Transmission lorentzian fitted parameters')
+            print(popt_transmission)
+
 
         # get reflection data
         self.cmd_interface.set('switch_ps_channel_output', 1)
@@ -86,6 +98,26 @@ class DataLogger:
         if autoscale:
             self.cmd_interface.set('na_commands', 'autoscale')
         self.cmd_interface.cmd('s21_iq_reflection_data', 'scheduled_log')
+        if fitting:
+            s11_iq = test_interface.get('s21_iq_reflection_data').payload.to_python()['value_cal']
+            s11_re, s11_im = np.array(s11_iq[::2]), np.array(s11_iq[1::2])
+            s11_pow = s11_re**2 + s11_im**2
+            s11_mag = np.sqrt(s11_pow)
+            s11_phase = np.unwrap(np.angle(s11_re+1j*s11_im))
+            popt_reflection, pcov_reflection = data_lorentzian_fit(s11_pow, freq, 'reflection')
+            perr_reflection = np.sqrt(np.diag(pcov_reflection))
+            print('Reflection lorentzian fitted parameters')
+            print(popt_reflection)
+            # Gam_res is reflection coeffient Gamma of the resonator
+            Gam_res_mag, Gam_res_phase = reflection_deconvolve_line(freq, s11_mag, s11_phase, popt_reflection[3])
+            # Calculates magnitude of Gamma_cavity by plugging resonant frequency into fitted function
+            Gam_res_mag_fo = np.sqrt(func_pow_reflected(popt_reflection[0], *popt_reflection)*1/popt_reflection[3])
+            Gam_res_interp_phase = interp1d(freq, Gam_res_phase, kind='cubic')
+            # calculate phase of Gamma_cavity at resonant frequency by interpolating
+            # data.
+            Gam_res_phase_fo = Gam_res_interp_phase(popt_reflection[0])
+            beta = calculate_coupling(Gam_res_mag_fo, Gam_res_phase_fo)
+            print("Antenna coupling : {}".format(beta))
         print('Setting na_measurement_status to stop_measurement')
         self.cmd_interface.set('na_measurement_status', 'stop_measurement')
 
