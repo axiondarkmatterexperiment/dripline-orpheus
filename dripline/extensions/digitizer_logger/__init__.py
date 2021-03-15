2071848a681321516a637533c555e03279754b8a
Raphael Cervantes <raphaelc@uw.edu>
2021-03-15 09:44:24 -0700

    first pass. adding integrated power

---
+++ dripline/extensions/digitizer_logger/__init__.py
@@ -0,0 +1,22 @@
+__all__ = []
+
+import pkg_resources
+
+import scarab
+a_ver = '0.0.0' #note that this is updated in the following block
+try:
+    a_ver = pkg_resources.get_distribution('entities_snapshotter_plugin').version
+    print('version is: {}'.format(a_ver))
+except:
+    print('fail!')
+    pass
+version = scarab.VersionSemantic()
+version.parse(a_ver)
+version.package = 'digitizer_logger__plugin'
+version.commit = '---'
+__all__.append("version")
+
+from .digitizer_logger import *
+from .digizer_logger import __all__ as __digitizer_logger_all
+__all__ += __digitizer_logger_all
+
---
+++ dripline/extensions/digitizer_logger/digitizer_logger.py
@@ -0,0 +1,36 @@
+from dripline.core import Interface, ThrowReply, get_return_codes_dict
+from dripline.implementations import PostgresSensorLogger
+import logging
+logger = logging.getLogger(__name__)
+import time
+
+__all__ = []
+
+__all__.append("PostgresDigitizerLogger")
+class PostgresDigitizerLogger(PostgresSensorLogger):
+    '''
+    Postgres logger for the digitizer service
+    '''
+    def __init__(self, integrated_power_endpoint, auths_file = None, **kwargs):
+        '''
+        '''
+        PostgresSensorLogger.__init__(self,**kwargs)
+        self._auths_file = auths_file
+        self.python_interface = Interface(dripline_config={'auth-file': self._auths_file})
+
+    def process_payload(self, a_payload, a_routing_key_data, a_message_timestamp):
+        this_data_table = self.sync_children[self.insertion_table_endpoint_name]
+        # combine data sources
+        insert_data = {'timestamp': a_message_timestamp}
+        insert_data.update(a_routing_key_data)
+        print(a_routing_key_data)
+        insert_data.update(a_payload.to_python())
+        logger.info(f"insert data are:\n{insert_data}")
+        if a_routing_key_data == 'fast_daq.medium_spectrum':
+            calculated_integrated_power = np.sum(insert_data['value_raw'])
+            self.python_interface.set(integrated_power_endpoint, calculated_integrated_power)
+            self.python_interface.cdm(integrated_power_endpoint, "scheduled_log")
+        # do the insert
+        this_data_table.do_insert(**insert_data)
+        logger.info("finished processing data")
+
