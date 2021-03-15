c02dbf3491379c702bbee1c0c235e0bd8a6b7d15
Raphael Cervantes <raphaelc@uw.edu>
2021-03-15 09:52:20 -0700

    fixed treatment of integrated power endpoint

--- dripline/extensions/digitizer_logger/digitizer_logger.py
+++ dripline/extensions/digitizer_logger/digitizer_logger.py
@@ -11,12 +11,12 @@ class PostgresDigitizerLogger(PostgresSensorLogger):
     '''
     Postgres logger for the digitizer service
     '''
-    def __init__(self, integrated_power_endpoint, auths_file = None, **kwargs):
+    def __init__(self, integrated_power_endpoint, **kwargs):
         '''
         '''
         PostgresSensorLogger.__init__(self,**kwargs)
-        self._auths_file = auths_file
-        self.python_interface = Interface(dripline_config={'auth-file': self._auths_file})
+        self._integrated_power_endpoint = integrated_power_endpoint
+        self._python_interface = Interface(dripline_config={'auth-file': self._auths_file})
 
     def process_payload(self, a_payload, a_routing_key_data, a_message_timestamp):
         this_data_table = self.sync_children[self.insertion_table_endpoint_name]
@@ -28,8 +28,8 @@ class PostgresDigitizerLogger(PostgresSensorLogger):
         logger.info(f"insert data are:\n{insert_data}")
         if a_routing_key_data == 'fast_daq.medium_spectrum':
             calculated_integrated_power = np.sum(insert_data['value_raw'])
-            self.python_interface.set(integrated_power_endpoint, calculated_integrated_power)
-            self.python_interface.cdm(integrated_power_endpoint, "scheduled_log")
+            self._python_interface.set(self._integrated_power_endpoint, calculated_integrated_power)
+            self._python_interface.cdm(self._integrated_power_endpoint, "scheduled_log")
         # do the insert
         this_data_table.do_insert(**insert_data)
         logger.info("finished processing data")
