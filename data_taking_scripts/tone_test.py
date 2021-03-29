9cb80f7e196f101b394d634526b9d6a1ea5dc6e9
Raphael Cervantes <raphaelc@uw.edu>
2021-03-29 07:56:28 -0700

    adding script to test tones with VNA

---
+++ data_taking_scripts/tone_test.py
@@ -0,0 +1,30 @@
+from data_logging import DataLogger
+from dripline.core import Interface
+import sys
+
+#setting up connection to dripline
+auths_file = '/etc/rabbitmq-secret/authentications.json'
+
+the_interface = Interface(dripline_config={'auth-file': auths_file})
+
+def tone_from_vna(frequency, power):
+    the_interface.set('na_center_freq', frequency)
+    the_interface.set('na_freq_span', 0)
+    the_interface.set('na_if_bw', 0)
+    the_interface.set('na_power', power)
+
+orpheus_motors = OrpheusMotors(auths_file, motors_to_move)
+logger = DataLogger(auths_file)
+
+gain = 50 #dB
+tone_frequency =  16.5e9
+target_tone_power = 28-gain #aim for 28 dBm at digitizer input. Do not exceed 34 dBm at digitizer input.
+
+vna_power = the_interface.get('na_power')
+
+if vna_power > target_tone_power:
+    print('VNA power cant go as low as needed. Exiting program')
+    sys.exit()
+#else:
+#    tone_with_vna(tone_frequency, target_tone_power)
+#    logger.digitize(injected_tone, if_center, digitization_time)
