678e0f70bae75b2641c4dc9e715888f3c14a3439
Raphael Cervantes <raphaelc@uw.edu>
2021-04-27 11:34:30 -0700

    faster vna

--- data_taking_scripts/axion_data_taking.py
+++ data_taking_scripts/axion_data_taking.py
@@ -48,10 +48,13 @@ the_interface.set('target_fo', starting_fo)
 
 #find initial VNA window for narrowscan measurements
 target_fo = the_interface.get('target_fo').payload.to_python()['value_cal']
+
 #get frequency span for narrowscan
 narrow_scan_start_freq = target_fo - narrow_scan_span_guess/2
 narrow_scan_stop_freq = target_fo + narrow_scan_span_guess/2
 resonant_freq_guess = data_logger.guess_resonant_frequency(narrow_scan_start_freq, narrow_scan_stop_freq, averaging_time = averaging_time_for_fo_guess_measurement)
+narrow_scan_start_freq_focus = target_fo-narrow_scan_span_focus/2
+narrow_scan_stop_freq_focus = target_fo+narrow_scan_span_focus/2
 the_interface.set('target_fo', resonant_freq_guess)
 target_fo = the_interface.get('target_fo').payload.to_python()['value_cal']
 
@@ -69,7 +72,7 @@ try:
             data_logger.log_transmission_switches(wide_scan_start_freq, wide_scan_stop_freq, sec_wait_for_na_averaging, 'axion data taking. widescan')
 
         #log narrowscan transmission
-        resonant_freq_guess = data_logger.guess_resonant_frequency(narrow_scan_start_freq_focus, narrow_scan_stop_freq_focus, averaging_time = averaging_time_for_fo_guess_measurement/3)
+        resonant_freq_guess = data_logger.guess_resonant_frequency(narrow_scan_start_freq_focus, narrow_scan_stop_freq_focus, averaging_time = averaging_time_for_fo_guess_measurement/4)
         the_interface.set('target_fo', resonant_freq_guess)
         narrow_scan_start_freq_focus = target_fo-narrow_scan_span_focus/2
         narrow_scan_stop_freq_focus = target_fo+narrow_scan_span_focus/2
