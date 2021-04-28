ce0b1d642bb1ee1e7d1f8d0fe4f1ef6c027ff73d
Raphael Cervantes <raphaelc@uw.edu>
2021-04-28 13:41:30 -0700

    get voltage uncertainty

---
+++ data_taking_scripts/get_vna_noisefloor.py
@@ -0,0 +1,19 @@
+from data_logging import DataLogger
+from dripline.core import Interface
+
+#setting up connection to dripline
+auths_file = '/etc/rabbitmq-secret/authentications.json'
+
+the_interface = Interface(dripline_config={'auth-file': auths_file})
+
+data_logger = DataLogger(auths_file)
+
+data_logger.switch_digitization_path()
+
+noise_floor_voltage = the_interface.get('s21_iq_transmission_data').payload.to_python()['value_cal']
+the_interface.cmd('s21_iq_transmission_data', 'scheduled_log')
+
+print(np.std(noise_floor_voltage))
+
+
+
