6e6c70e9deb96915e65fd381a81ecf0cf347e754
Raphael Cervantes <raphaelc@uw.edu>
2021-05-26 15:23:25 -0700

    sync child endpoints

--- dripline/extensions/agilent34970A/agilent34970A.py
+++ dripline/extensions/agilent34970A/agilent34970A.py
@@ -19,26 +19,33 @@ class MuxerService(EthernetSCPIService):
         self.send_to_device('ABOR;*CLS;*OPC?')
 
         ch_scan_list = list()
-        for child in self.endpoints:
+        logger.info(self.sync_children)
+        for child in self.sync_children:
 
-            if not isinstance(self.endpoints[child], MuxerGetEntity):
+            logger.info(child)
+            if not isinstance(self.sync_children[child], MuxerGetEntity):
+                logger.info(child)
+                logger.info('help0')
                 continue
-            elif self.endpoints[child].conf_str:
+            elif self.sync_children[child].conf_str:
                 # check if the configuration command associated with the endpoint is valid.
-                error_data = self.send([self.endpoints[child].conf_str+';*OPC?',\
-                                        'SYST:ERR?'])
+                error_data = self.send(self.sync_children[child].conf_str+';*OPC?; SYST:ERR?')
+                logger.info('help')
                 if error_data != '1;+0,"No error"':
                     logger.critical('Error detected; cannot configure muxer')
                     raise ThrowReply('{} when attempting to configure endpoint named "{}"'.format(error_data,child))
-            ch_scan_list.append(str(self.endpoints[child].ch_number))
-            self.endpoints[child].log_interval = self.scan_interval
+                logger.info('help2')
+            ch_scan_list.append(str(self.sync_children[child].ch_number))
+            self.sync_children[child].log_interval = self.scan_interval
 
+        logger.info('help3')
         scan_list_cmd = 'ROUT:SCAN (@{})'.format(','.join(ch_scan_list))
-        self.send([scan_list_cmd+';*OPC?',\
-                   'TRIG:SOUR TIM;*OPC?',\
-                   'TRIG:COUN INF;*OPC?',\
-                   'TRIG:TIM {};*OPC?'.format(self.scan_interval),\
-                   'INIT;*ESE?'])
+        logger.info('help4')
+        self.send_to_device(scan_list_cmd+';*OPC?;'+\
+                   'TRIG:SOUR TIM;*OPC?;'+\
+                   'TRIG:COUN INF;*OPC?;'+\
+                   'TRIG:TIM {};*OPC?;'.format(self.scan_interval)+\
+                   'INIT;*ESE?;')
 
 
     def __init__(self, scan_interval=0,**kwargs):
@@ -50,7 +57,9 @@ class MuxerService(EthernetSCPIService):
             raise ThrowReply("scan interval must be > 0")
             #raise exceptions.DriplineValueError("scan interval must be > 0")
         self.scan_interval = scan_interval
-        self.configure_scan()
+        logger.info(self.sync_children)
+        logger.info(self)
+        #self.configure_scan()
 
 __all__.append("MuxerGetEntity")
 class MuxerGetEntity(Entity):
