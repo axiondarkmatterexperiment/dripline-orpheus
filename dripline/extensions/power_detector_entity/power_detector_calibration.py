9c401ec8fc1e4a145b215cdde198d290c3d35588
Raphael Cervantes <raphaelc@uw.edu>
2021-08-16 11:43:42 -0700

    fixing function call

--- dripline/extensions/power_detector_entity/power_detector_calibration.py
+++ dripline/extensions/power_detector_entity/power_detector_calibration.py
@@ -27,7 +27,7 @@ def _power_from_calibration_file(voltage, calibration_file):
 def zx47_50_cal(voltage):
     '''calibration for zx47-50 power detector'''
     calibration_file = calibration_dir + '/zx4750_calibration.csv'
-    interpolated_temperature = _temp_from_calibration_file(voltage, calibration_file)
+    interpolated_temperature = _power_from_calibration_file(voltage, calibration_file)
     return interpolated_temperature
 __all__.append("zx47_50_cal")
 
