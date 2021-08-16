0c4456882ff6248db87fdb22249e1e8de3a9fa4c
Raphael Cervantes <raphaelc@uw.edu>
2021-08-16 12:27:51 -0700

    fixed bad index

--- dripline/extensions/power_detector_entity/power_detector_calibration.py
+++ dripline/extensions/power_detector_entity/power_detector_calibration.py
@@ -19,16 +19,16 @@ def _power_from_calibration_file(voltage, calibration_file):
     #TODO throw error if resistance is anything but a float.
     M = np.loadtxt(calibration_file, delimiter = ',')
     voltage_cal = M[:,0]
-    power_cal = M[:,0]
+    power_cal = M[:,1]
     interpolated_function = interp1d(voltage_cal, power_cal, kind = 'cubic')
     interpolated_power = float(interpolated_function(voltage)) # interpolation returns an array. Dripline can't handle numpy array. So I cast it to a float.
     return interpolated_power
 
 def zx47_50_cal(voltage):
-    '''calibration for zx47-50 power detector'''
+    '''calibration for zx47-50 power detector. Output in dBm'''
     calibration_file = calibration_dir + '/zx4750_calibration.csv'
-    interpolated_temperature = _power_from_calibration_file(voltage, calibration_file)
-    return interpolated_temperature
+    interpolated_power = _power_from_calibration_file(voltage, calibration_file)
+    return interpolated_power
 __all__.append("zx47_50_cal")
 
 
