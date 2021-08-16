323f14601cddd21e74d6a9438012c29d8fafd469
Raphael Cervantes <raphaelc@uw.edu>
2021-08-16 10:32:38 -0700

    adding PowerDetectorEntity

---
+++ dripline/extensions/power_detector_entity/power_detector.py
@@ -0,0 +1,23 @@
+from dripline.core import Entity, calibrate, ThrowReply
+from dripline.implementations import EthernetSCPIService
+import logging
+from .power_detector_calibration import zx47_50_cal
+
+logger = logging.getLogger(__name__)
+
+__all__ = []
+
+__all__.append("PowerDetectorEntity")
+class PowerDetectorEntity(Entity):
+    '''
+    Entity with power detector calibration
+    '''
+
+    @calibrate([zx47_50_cal])
+    def on_get(self):
+        return self._value
+
+    def on_set(self, value):
+        raise ThrowReply('setting not available for {}'.format(self.name))
+
+
