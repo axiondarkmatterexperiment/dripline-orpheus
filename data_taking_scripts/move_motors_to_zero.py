6ee21a48472de8c35f1ed17bc2589aeaf997d95f
James Sinnis <jsinnis@nlwymmd.admx>
2021-09-14 14:46:15 -0700

    changed the config files used to be up-to-date

--- data_taking_scripts/move_motors_to_zero.py
+++ data_taking_scripts/move_motors_to_zero.py
@@ -1,9 +1,16 @@
 from dripline.core import Interface
 import time
-import common_functions
+from motor import OrpheusMotors
+import yaml
 
 auths_file = '/etc/rabbitmq-secret/authentications.json'
 the_interface = Interface(dripline_config={'auth-file': auths_file})
+config_file = open("modemap_config.yaml")
+configs = yaml.load(config_file, Loader = yaml.FullLoader)
+general_mechanical_configs = configs['general_configs']
+locals().update(general_mechanical_configs)
+
+orpheus_motors = OrpheusMotors(auths_file, motors_to_move)
 
 the_interface.set('curved_mirror_move_to_position', 0)
 the_interface.set('bottom_dielectric_plate_move_to_position', 0)
