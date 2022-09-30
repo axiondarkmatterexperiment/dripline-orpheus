27c0c468237cedb1a2d17ab61c55a02b238c3980
jtsinnis <77714881+jtsinnis@users.noreply.github.com>
2022-09-30 14:16:05 -0700

    snapshot after narrow scan zoom-in
    addressing an issue Raphael pointed out.

--- data_taking_scripts/axion_data_taking.py
+++ data_taking_scripts/axion_data_taking.py
@@ -98,12 +98,11 @@ try:
 
         the_interface.set('axion_record_spectrum_status', 'start_measurement')
 
-        the_interface.cmd('axion_data_taking_short_snapshot', 'log_entities')
-
         data_logger.log_transmission_switches(narrow_scan_start_freq_focus, narrow_scan_stop_freq_focus, sec_wait_for_na_transmission_averaging, fitting = True, transmission_endpoint = 's21_iq_transmission_data')
 
         data_logger.log_reflection_switches(narrow_scan_start_freq_focus, narrow_scan_stop_freq_focus, sec_wait_for_na_reflection_averaging, fitting = True, reflection_endpoint = 's21_iq_reflection_data')
 
+        the_interface.cmd('axion_data_taking_short_snapshot', 'log_entities') #Moved here, so that the axion data taking snapshot is taken after zooming in around resonance.
         #take axion data
         measured_fo = the_interface.get('f_transmission').payload.to_python()['value_cal']
         data_logger.digitize(measured_fo, if_center, digitization_time, fft_bin_width, log_power_monitor = True, disable_motors = disable_motors_while_digitizing)
